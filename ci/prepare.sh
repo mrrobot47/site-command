05d9f95678cace1977a938c52c99b008ca322bfb
Riddhesh Sanghvi <riddhesh.sanghvi@rtcamp.com>
2018-07-11 16:29:17 +0530

    Add tests prepration shell script

---
+++ ci/prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# called by Travis CI
+
+cd ..
+git clone https://github.com/EasyEngine/easyengine.git easyengine --depth=1
+cd easyengine
+rm -r features
+cp -R ../$TEST_COMMAND/features .
+rm -r vendor/easyengine/$TEST_COMMAND
+cp -R ../$TEST_COMMAND vendor/easyengine/
+composer update -vvv
